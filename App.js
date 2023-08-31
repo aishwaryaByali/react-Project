bd0c72d1637796108783ea6767118eab8cb9318b
aishwaryaByali <b.aishwarya4@gmail.com>
2023-09-01 00:29:32 +0530

    resto card data

--- App.js
+++ App.js
@@ -4,15 +4,86 @@
 //   "Hello World from React!"
 // );
 // console.log(reactheading);
-const heading = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Hello World!"),
-    React.createElement("h1", {}, "Hello World! h1"),
-  ])
-);
-console.log(heading);
-const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-reactRoot.render(heading);
+/**
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - Search
+ *  - RestaurantContainer
+ *    - RestaurantCard
+ * Footer
+ *  - Copyright
+ *  - links
+ *  - Address
+ *  - Contact
+ */
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+          alt="food-app"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const RestoCard = (props) => {
+  return (
+    <div className="resto-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://www.bakingo.com/blog/wp-content/uploads/2023/04/15-best-restaurants-in-bangalore.jpg"
+      />
+      <h3>{props.name}</h3>
+      <h5>Biryani</h5>
+      <h5>4.3 starts</h5>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="restro-Container">
+        <RestoCard name="Meghana Foods" />
+        <RestoCard name="KFC" />
+        <RestoCard name="Hotel 1" />
+        {/* <RestoCard />
+        <RestoCard />
+        <RestoCard />
+        <RestoCard />
+        <RestoCard />
+        <RestoCard />
+        <RestoCard />
+        <RestoCard />
+        <RestoCard /> */}
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div id="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
+reactRoot.render(<AppLayout />);
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-#Namaste React
+# Namaste React
+
+# .gitignore
+
+- anything which can be regenerated, should not be pushed to github
+- eg., dist, .parcel-cache, node_modules
+
+# npx parcel build index.html
+
+- it is to build bundle for production. remove "main": "App.js", from package.json, because the starting point is already given in the command (index.html)
+
+# parcel
+
+- Dev build
+- Local Server
+- HMR -> Hot Module Replacement
+- File Watching Algorithm - written in C++
+- faster development experience - using caching
+- Image Optimization
+- Minification
+- Bundle all the files
+- Compress the files
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older bundling
+- Error Handling
+- Https
+- Tree shaking - remove unused code
+- Different dev and prod bundles
